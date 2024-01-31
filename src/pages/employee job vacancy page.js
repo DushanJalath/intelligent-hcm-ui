4b7f8b071502af6df3c734fbc9f8d65f093f7260
Akash <akashsivakumar09@gmail.com>
2024-02-01 02:09:31 +0530

    employeejobvacancypageadded

---
+++ src/pages/employee job vacancy page.js
@@ -0,0 +1,25 @@
+import Jobvacancy from "../components/newjobvacancies.js"
+import "./employee job vacancy page.css"
+
+
+function App() {
+    return (
+  <>
+   <div className="app-container">
+      <div className="navbar">
+        
+      </div>
+      <div className="main-content">
+      <Jobvacancy title="New Job Vacancies"/> 
+      </div>
+      
+    </div>
+  </>
+    );
+   
+  
+  }
+  
+  
+  
+  export default App;

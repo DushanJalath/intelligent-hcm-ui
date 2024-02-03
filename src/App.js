83bfa2205114c4d4ecb394ffd55a833a4011a631
Malinda Rathnayaka <122790328+Rathnayaka00@users.noreply.github.com>
2024-02-03 19:46:43 +0530

    Add LeaveApprove Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import RootLayout from "./components/RootLayout";
-import { Route, Routes } from "react-router-dom";
-import TimeReporting from "./components/TimeReporting";
-import Settings from "./components/Settings";
+/* //NavBar Code
+import RootLayout from "./components/RootLayout"; //NavBar
+import { Route, Routes } from "react-router-dom"; //NavBar
+import TimeReporting from "./components/TimeReporting"; //NavBar
+import Settings from "./components/Settings"; //NavBar
+
+
 
 const App = () => {
-  return (
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<TimeReporting />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-      </RootLayout>
-  );
+    return (
+        <RootLayout>
+            <Routes>
+                <Route path="/" element={<TimeReporting />} />
+                <Route path="/settings" element={<Settings />} />
+            </Routes>
+        </RootLayout>
+    );
 };
+
+export default App;
+ */
+
+
+
+import React from 'react';
+import LeaveApprove from '../src/components/LeaveApprove';
+
+
+function App() {
+    return (
+        <div className='App'>
+            <LeaveApprove />
+        </div>
+    );
+}
 
 export default App;

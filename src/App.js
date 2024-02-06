ac23e7eb499ee9ccbe14f9e5426f4db0c99fd5f3
Malinda Rathnayaka <122790328+Rathnayaka00@users.noreply.github.com>
2024-02-06 15:46:18 +0530

    Add New Candidate Submit form Page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { Route, Routes } from "react-router-dom"; //AllNavBars
 import TimeReporting from "./components/TimeReporting"; //AllNavBars
 import Settings from "./components/Settings"; //AllNavBars
 // // import RootLayout from "./components/RootLayout"; //EmployeeNavBar
-// // import ManagerRootLayout from "./components/ManagerRootLayout"; //ManagerNavBar
-import HRRootLayout from "./components/HRRootLayout"; //HRNavBar
+import ManagerRootLayout from "./components/ManagerRootLayout"; //ManagerNavBar
+// import HRRootLayout from "./components/HRRootLayout"; //HRNavBar
 // import NewCandidateRootLayout from "./components/NewCandidateRootLayout";
 
 /*
@@ -26,9 +26,9 @@ export default App;
 
 
 
-// import EmpLeave from "./components/EmpLeave";
-// import RemainingLeaves from "./components/RemainingLeaves";
-// import LeaveApprove from "./components/LeaveApprove";
+import EmpLeave from "./components/EmpLeave";
+import RemainingLeaves from "./components/RemainingLeaves";
+import LeaveApprove from "./components/LeaveApprove";
 
 
 function App() {
@@ -44,22 +44,20 @@ function App() {
             {/*</RootLayout>*/}
 
 
-
-            {/*Manager NavBar*/}
-            {/*<ManagerRootLayout>*/}
-            {/*    <Routes>*/}
-            {/*        <Route path="/" element={<TimeReporting />} />*/}
-            {/*        <Route path="/settings" element={<Settings />} />*/}
-            {/*    </Routes>*/}
-            {/*</ManagerRootLayout>*/}
-
-
-            <HRRootLayout>
+            <ManagerRootLayout>
                 <Routes>
                     <Route path="/" element={<TimeReporting />} />
                     <Route path="/settings" element={<Settings />} />
                 </Routes>
-            </HRRootLayout>
+            </ManagerRootLayout>
+
+
+            {/*<HRRootLayout>*/}
+            {/*    <Routes>*/}
+            {/*        <Route path="/" element={<TimeReporting />} />*/}
+            {/*        <Route path="/settings" element={<Settings />} />*/}
+            {/*    </Routes>*/}
+            {/*</HRRootLayout>*/}
 
 
             {/*<NewCandidateRootLayout>*/}
@@ -70,9 +68,9 @@ function App() {
             {/*</NewCandidateRootLayout>*/}
 
 
-            {/*<EmpLeave title="Request Leave"/>*/}
-            {/*<RemainingLeaves />*/}
-            {/*<LeaveApprove />*/}
+            <EmpLeave title="Request Leave"/>
+            <RemainingLeaves />
+            <LeaveApprove />
             {/*<EmployeeSubmitForm title="sss" />*/}
             {/*<NewCandidateSubmitForm />*/}
 

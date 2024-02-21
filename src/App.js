6bb65e4caa7e687768850717611b8f2771b62e7e
DushanJalath <dushanprabash@gmail.com>
2024-02-21 23:16:29 +0530

    solving

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import UserRegistration from './components/UserRegistration';
 
 
 function App() {
     return (
-        <div className='App'>
-            <UserRegistration title="User Registration"/>
-        </div>
+        <>
+        </>
     );
 
 }

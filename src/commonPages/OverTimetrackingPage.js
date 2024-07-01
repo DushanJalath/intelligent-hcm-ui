0c272752308fec45fcd2db1ab95df4d80a431075
DushanJalath <dushanprabash@gmail.com>
2024-07-02 01:43:57 +0530

    chat bot added to some UIs

--- src/commonPages/OverTimetrackingPage.js
+++ src/commonPages/OverTimetrackingPage.js
@@ -5,10 +5,22 @@ import TimeAndDate from "../components/TimeAndDate.js"
 import "../styles/employeeOvertimeTrackingPage.css"
 import Sidebar from "../components/Sidebar";
 import ManagerSidebar from "../components/ManagerSidebar";
-
+import buttonImage from "../assets/bot.png";
+import Modal from "react-modal";
+import Chatbot from '../components/Chatbot.js';
+import React, { useState } from 'react';
 
 export default function OverTimetrackingPage (){
     const userType = localStorage.getItem('userType');
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
     return (
         
         <div className="appcontainer">
@@ -24,6 +36,38 @@ export default function OverTimetrackingPage (){
 
             <div className="column2">
                 <Monthlyreport title="Monthly report"/>
+            </div>
+            <div className='chat-bot-icon'>
+                <button className="round-button-chatbot" onClick={openModal}>
+                    <img src={buttonImage} alt="Button" className="button-image-chatbot" />
+                </button>
+                <Modal 
+                    isOpen={modalIsOpen} 
+                    onRequestClose={closeModal}
+                    style={{
+                        content:{
+                            top: '50%',
+                            left: '40%',
+                            right: '20%',
+                            bottom: 'auto',
+                            marginRight: '100px',
+                            transform: 'translate(-50%, -50%)',
+                            width: '80%', // Adjusted width for better responsiveness
+                            maxWidth: '600px',
+                            height: '80vh', // Adjusted height to fit better in the viewport
+                            padding: '0', // Remove default padding
+                            border: 'none', // Remove default border
+                            borderRadius: '10px', // Match chatbot border-radius
+                            overflow: 'hidden' 
+                        },
+                        overlay: {
+                            backgroundColor: 'rgba(0, 0, 0, 0.5)' // Add overlay color
+                        }
+                
+                    }}
+                >
+                    <Chatbot />
+                </Modal>
             </div>
         </div>
     )

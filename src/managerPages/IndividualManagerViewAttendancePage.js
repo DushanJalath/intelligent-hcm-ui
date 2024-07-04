b2f7d425befd8e7555834d603eef7f58893987ab
Malinda Rathnayaka <122790328+Rathnayaka00@users.noreply.github.com>
2024-07-04 13:27:56 +0530

    Set Routes

--- src/managerPages/IndividualManagerViewAttendancePage.js
+++ src/managerPages/IndividualManagerViewAttendancePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CalendarComponent from '../components/ViewAttendanceCalender.js';
-import Sidebar from "../components/Sidebar.jsx";
+import ManagerSidebar from "../components/ManagerSidebar";
 import '../styles/individualEmployeeViewAttendancePage.css'
 import WeeklyViewAttendanceBarchart from '../components/WeeklyViewAttendanceBarchart.js'
 import YearlyWorkHourBarchart from '../components/YearlyWorkHourBarchart.js'
@@ -9,7 +9,7 @@ export default function ViewAttendancePage (){
   return (
     <div className="app-container">
       <div className="navbar">
-            <Sidebar/>
+            <ManagerSidebar/>
       </div>
       <div className="main-content">
               <div className="main-content-firstrow">
